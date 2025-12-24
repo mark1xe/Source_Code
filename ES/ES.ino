2365902c3beb8cc046178501932a471ce0528023
mark1xe <buiminhphuong0211@gmail.com>
2025-12-24 23:38:28 +0700

    water pump

--- ES/ES.ino
+++ ES/ES.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#define RELAY_PIN   27
+#define BUTTON_PIN  32
+#define LED_PIN     2
 
+bool relayState = true;
+bool lastButtonState = LOW;
+bool buttonState = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool reading = digitalRead(BUTTON_PIN);
 
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if (millis() - lastDebounceTime > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        relayState = !relayState;
+        digitalWrite(RELAY_PIN, relayState ? HIGH : LOW);
+        digitalWrite(LED_PIN, relayState ? HIGH : LOW);
+      }
+    }
+  }
+
+  lastButtonState = reading;
 }
